0702f745a3901e0e314a627872a3b353eabc94f6
Chumki Dutta Gupta <chumkidutta12@gmail.com>
2025-11-12 13:45:36 +0530

    Practice working with OpenCSV library

---
+++ src/main/java/com/demo/csv/ReadCSVFile.java
@@ -0,0 +1,46 @@
+package com.demo.csv;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class ReadCSVFile {
+
+	public static void main(String[] args) throws IOException, CsvException {
+		// Code to read the csv file in java
+		
+		/*
+		 File csvFile= new File("C:\\Users\\dell\\Desktop\\SDET with Jatin\\JavaModuleCode\\PhoenixTestAutomationFramework\\src\\main\\resources\\testData\\LoginCreds.csv");
+		 FileReader fr=new FileReader(csvFile);
+		 */
+		
+		
+		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds");	
+		InputStreamReader isr= new InputStreamReader(is);
+		
+		
+		CSVReader csvReader= new CSVReader(isr);		//csvReader Constructor 
+		//Requires a Reader
+		List<String[]> dataList=csvReader.readAll();
+		
+		for(String[] dataArray: dataList)
+		{
+			//for(String data: dataArray)
+			//{
+				System.out.print(dataArray[0]);
+				System.out.print(dataArray[0]);
+			//}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
+
